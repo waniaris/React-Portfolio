8c0111055f91d92727e99560a6a732a8dfe82a2b
waniaris <norhazwaniaris@gmail.com>
2025-08-06 21:15:53 +0100

    added pages structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// App.js
+import { useState } from 'react';
+import Layout from './components/Layout';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [page, setPage] = useState('home');
+
+  // TODO: How doe sthe Layout component know which page to render?
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Layout selectedPage={page} onSetPage={setPage}>
+
+      {page === 'home' && <HomePage />}
+      {page === 'about' && <AboutPage />}
+      {page === 'contact' && <ContactPage />}
+
+    </Layout>
+  );
 }
 
-export default App
+export default App;
